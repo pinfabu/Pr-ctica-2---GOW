0d92bb52a60427b75813a7dd61518292439d7b9a
Juan carlos <pinfabu@gmail.com>
2024-03-09 09:18:55 -0600

    Advance

---
+++ GOW/View/WeaponCell.swift
@@ -0,0 +1,23 @@
+//
+//  WeaponCell.swift
+//  GOW
+//
+//  Created by Juan Carlos Díaz Valenzuela on 09/03/24.
+//
+
+import UIKit
+
+class WeaponCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
